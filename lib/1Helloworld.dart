865b7795544611483db11d617863a8ffb3afb658
Nikhildeshmukh07 <113224493+Nikhildeshmukh07@users.noreply.github.com>
2023-08-24 12:33:50 +0530

    first commit from sstpl 24-08-2023 12:33

--- lib/1Helloworld.dart
+++ lib/1Helloworld.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 
-
 class HelloWorldScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello World'),
+        title: const Text('Hello World'),
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
       ),
-      body: Center(
+      body: const Center(
         child: Text('Hello, World!'),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learn/3TutorialHome.dart';
+import '4.Handling gestures.dart';
+import '5Changingwidgetsinresponsetoinput.dart';
 import 'Dashboard.dart';
 
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,6 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: DashboardScreen(),
+    //  home : Counter2(),
     );
   }
 }
